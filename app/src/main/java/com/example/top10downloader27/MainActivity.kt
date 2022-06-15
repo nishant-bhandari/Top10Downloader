b7bab764398220009a81cff703c2be5fddedcc1d
NISHANT BHANDARI <nishantbhandari2002@gmail.com>
2022-06-15 22:30:31 +0530

    Top 10 Downloader App

---
+++ app/src/main/java/com/example/top10downloader27/MainActivity.kt
@@ -0,0 +1,164 @@
+package com.example.top10downloader27
+
+import android.content.Context
+import android.os.AsyncTask
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import java.net.URL
+import kotlin.properties.Delegates
+
+class FeedEntry {
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
+    override fun toString(): String {
+        return """
+            name : $name
+            artist : $artist
+            realeseDate : $releaseDate
+            imageUrl : $imageURL
+        """.trimIndent()
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+    private var downloadData: DownloadData? = null
+    private var feedUrl: String = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml"
+    private var feedLimit = 10
+
+    private var feedCachedUrl = "INVALIDATED"
+    private val STATE_URL = "feedUrl"
+    private val STATE_LIMIT = "feedLimit"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"onCreate: Called")
+        if(savedInstanceState != null){
+            feedUrl = savedInstanceState.getString(STATE_URL)!!
+            feedLimit = savedInstanceState.getInt(STATE_LIMIT)
+        }
+
+        downloadUrl(feedUrl.format(feedLimit))
+        Log.d(TAG, "onCreate: Done")
+
+    }
+
+    private fun downloadUrl(feedUrl: String) {
+        if(feedUrl != feedCachedUrl) {
+            Log.d(TAG, "downloadUrl: Starting AsyncTask")
+            downloadData = DownloadData(this, findViewById(R.id.xmlListView))
+            // this statement is used because AsyncTask can be only executed only once
+            downloadData?.execute(feedUrl)          //safe call
+            feedCachedUrl = feedUrl
+            Log.d(TAG, "downloadUrl AsyncTask: done")
+        }
+        else {
+            Log.d(TAG,"downloadUrl: -URL not changed")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.feeds_menu, menu)
+        if(feedLimit == 10)
+            menu?.findItem(R.id.mnu10)?.isChecked = true
+        else
+            menu?.findItem(R.id.mnu25)?.isChecked = true
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.mnufree ->
+                feedUrl =
+                    "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml"
+            R.id.mnuPaid ->
+                feedUrl =
+                    "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=%d/xml"
+            R.id.mnuSongs ->
+                feedUrl =
+                    "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=%d/xml"
+            R.id.mnuAlbum ->
+                feedUrl =
+                    "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topalbums/limit=%d/xml"
+            R.id.mnuMovies ->
+                feedUrl =
+                    "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/limit=%d/xml"
+            R.id.mnu10, R.id.mnu25 -> {
+                if (!item.isChecked) {
+                    item.isChecked = true
+                    feedLimit = 35 - feedLimit
+                    Log.d(
+                        TAG,
+                        "onOptionsItemSelected: ${item.title} setting feedLimit to ${feedLimit}"
+                    )
+                } else {
+                    Log.d(TAG, "onOptionItemSelected: ${item.title} and feedLimit is Unchanged")
+                }
+            }
+            R.id.mnuRefresh -> feedCachedUrl = "INVALIDATED"
+            else ->
+                feedUrl = (return super.onOptionsItemSelected(item))
+        }
+        downloadUrl(feedUrl.format(feedLimit))
+        return true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_URL,feedUrl)
+        outState.putInt(STATE_LIMIT,feedLimit)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        downloadData?.cancel(true)
+    }
+
+    companion object {
+        private class DownloadData(context: Context, listView: ListView) :
+            AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+            var propContext: Context by Delegates.notNull()
+            var propListView: ListView by Delegates.notNull()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
+
+            override fun onPostExecute(result: String) {
+                super.onPostExecute(result)
+                val parseApplications = ParseApplications()
+                parseApplications.parse(result)
+                val feedAdapter =
+                    FeedAdapter(propContext, R.layout.list_record, parseApplications.applications)
+                propListView.adapter = feedAdapter
+            }
+
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: Parameter is ${url[0]}")
+                val rssFeed = downloadXMl(url[0])
+                if (rssFeed.isEmpty()) {
+                    Log.d(TAG, "doInBackground: Error downloading")
+                }
+                return rssFeed
+            }
+
+            private fun downloadXMl(urlPath: String?): String {
+                return URL(urlPath).readText()
+            }
+        }
+    }
+}
